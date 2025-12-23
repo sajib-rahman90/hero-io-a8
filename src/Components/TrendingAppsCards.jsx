e83210f9e14117cdf1180386543c7726efc671a5
sajib-rahman90 <sajibrahman0090@gmail.com>
2025-12-23 17:31:18 +0600

    added images link in json data files also designed apps cards in projects

--- src/Components/TrendingAppsCards.jsx
+++ src/Components/TrendingAppsCards.jsx
@@ -1,20 +1,29 @@
+import { Download, StarIcon } from "lucide-react";
 import React from "react";
 
 const TrendingAppsCards = ({ app }) => {
-  const { title, downloads, ratingAvg } = app;
+  const { title, downloads, ratingAvg, image } = app;
   return (
-    <div className="card bg-base-100 shadow-sm p-2">
-      <figure className="rounded-s-sm">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-        />
+    <div className="card bg-base-100 shadow-sm p-2 space-y-2.5">
+      <figure className="rounded-[10px]">
+        <img className="w-full h-56 object-cover " src={image} alt="Shoes" />
       </figure>
       <div className="space-y-3">
-        <h2 className="card-title">{title}</h2>
+        <h2 className="card-title text-[#001931] text-xl font-medium">
+          {title}
+        </h2>
         <div className="card-actions justify-between">
-          <div className="badge badge-outline">{downloads}</div>
-          <div className="badge badge-outline">{ratingAvg}</div>
+          <div className="py-1.5 px-3 rounded-sm bg-[#f1f5e8]">
+            <span className="text-[#00d390] flex gap-1.5">
+              <Download /> {downloads}
+            </span>
+          </div>
+          <div className="py-1.5 px-3 rounded-sm bg-[#fff0e1]">
+            <span className="text-[#ff8811] flex gap-1.5">
+              <StarIcon />
+              {ratingAvg}
+            </span>
+          </div>
         </div>
       </div>
     </div>
--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -15,13 +15,16 @@ const Apps = () => {
       <div className="w-11/12 mx-auto space-y-10 items-center text-center py-20">
         <div className="text-center space-y-4">
           <h1 className="text-5xl font-bold">Our All Applications</h1>
-          <p className="text-xl font-normal">
+          <p className="text-xl text-[#627382] font-normal">
             Explore All Apps on the Market developed by us. We code for Millions
           </p>
         </div>
         <div className="flex justify-between">
-          <h1>
-            <span>({searchedApps.length})</span> Apps Found
+          <h1 className="text-[#001931 text-2xl font-semibold]">
+            <span className="text-[#001931 text-2xl font-semibold]">
+              ({searchedApps.length})
+            </span>
+            Apps Found
           </h1>
 
           <label className="input">
